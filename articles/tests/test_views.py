23f55de9c5d633c6ef5d7266e751b0058ccc4f84
Voldemat <77781574+Voldemat@users.noreply.github.com>
2021-03-30 18:59:39 +0300

    'add_ArticleGalleryViewSetTestCase'

--- articles/tests/test_views.py
+++ articles/tests/test_views.py
@@ -20,35 +20,23 @@ from ..views import (
 	GalleryViewSet,
 	ArticleGalleryDetailView,
 )
-article_test_data = {
-	'title':'Space',
-	'text':'Something cool',
-	'dsc':'sdfds',
-	'preview_img':'dfsfd.img',
-}
-article_test_data_2 = {
-	'title':'Spdace',
-	'text':'Somedthing cool',
-	'dsc':'sdfdds',
-	'preview_img':'dfsfdd.img',
-}
+
+from .testdata import Data
+
 class ArticleViewSetTestCase(TestCase):
 	def setUp(self):
 		self.client = APIClient()
+
 		self.article_list = [
-			Article.objects.create(**article_test_data),
-			Article.objects.create(**article_test_data_2),
-
+			Article.objects.create(**Data.article_test_data),
+			Article.objects.create(**Data.article_test_data_2),
 		]
-		self.detail_instance = self.article_list[0]
+
 		self.list_response = self.client.get('/api/v1/articles/', format = 'json', content_type = 'application/json')
+
+
+		self.detail_instance = self.article_list[0]
 		self.detail_response = self.client.get(f'/api/v1/articles/{self.detail_instance.id.__str__()}/')
-
-
-
-		# self.article_list.append(
-		# 	Article.objects.create(**article_test_data)
-		# )
 
 
 	def test_status_code(self):
@@ -71,9 +59,41 @@ class ArticleViewSetTestCase(TestCase):
 
 
 
-
 class GalleryViewSet(TestCase):
-	pass
+	def setUp(self):
+		self.client = APIClient()
+
+		self.gallery_list = [
+			Article.objects.create(**Data.article_test_data).gallery,
+			Article.objects.create(**Data.article_test_data_2).gallery,
+		]
+		
+		self.list_response = self.client.get('/api/v1/galleries/', format = 'json', content_type = 'application/json')
+
+
+		self.detail_instance = self.gallery_list[0]
+
+		self.detail_response = self.client.get(f'/api/v1/galleries/{self.detail_instance.id.__str__()}/')
+
+	def test_status_code(self):
+		self.assertEqual(self.list_response.status_code, 200)
+		self.assertEqual(self.detail_response.status_code, 200)
+
+	def test_list_response(self):
+		self.assertTrue(
+			search_objects_from_datalist(
+				'id', # search key name
+				[gallery.id.__str__() for gallery in self.gallery_list], # value list
+				self.list_response.json() # datalist:List[dict]
+			)
+		)
+
+	def test_detail_response(self):
+		self.assertEqual(
+			self.detail_instance.id.__str__(),
+			self.detail_response.json()['id']
+		)
+
 
 class ArticleGalleryDetailView(TestCase):
 	pass
---
+++ articles/tests/testdata.py
@@ -0,0 +1,13 @@
+class Data:
+    article_test_data = {
+        'title':'Space',
+        'text':'Something cool',
+        'dsc':'sdfds',
+        'preview_img':'dfsfd.img',
+    }
+    article_test_data_2 = {
+        'title':'Spdace',
+        'text':'Somedthing cool',
+        'dsc':'sdfdds',
+        'preview_img':'dfsfdd.img',
+    }
